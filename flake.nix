1cd0e1f4075b661375b427ff8aac7bb577463aca
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:29 -0300

    chore: re index clang

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."clang-develop".dir   = "develop";
+  inputs."clang-develop".owner = "nim-nix-pkgs";
+  inputs."clang-develop".ref   = "master";
+  inputs."clang-develop".repo  = "clang";
+  inputs."clang-develop".type  = "github";
+  inputs."clang-develop".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."clang-develop".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."clang-v0_59".dir   = "v0_59";
+  inputs."clang-v0_59".owner = "nim-nix-pkgs";
+  inputs."clang-v0_59".ref   = "master";
+  inputs."clang-v0_59".repo  = "clang";
+  inputs."clang-v0_59".type  = "github";
+  inputs."clang-v0_59".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."clang-v0_59".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
